16d56b367c659bd03369444003bb58b78867a3b0
markozou <zou@zous-MacBook-Pro.local>
2014-11-03 02:27:11 +0800

    lecture1

---
+++ lecture1/lecture1Tests/lecture1Tests.m
@@ -0,0 +1,28 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface lecture1Tests : XCTestCase
+
+@end
+
+@implementation lecture1Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+
+    }];
+}
+
+@end
