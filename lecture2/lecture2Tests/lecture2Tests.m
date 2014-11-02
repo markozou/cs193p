214fb5f0e28bcc6c3f495db2736181a11ad7caa8
markozou <zou@zous-MacBook-Pro.local>
2014-11-03 02:41:14 +0800

    Initial lecture2

---
+++ lecture2/lecture2Tests/lecture2Tests.m
@@ -0,0 +1,28 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface lecture2Tests : XCTestCase
+
+@end
+
+@implementation lecture2Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+
+    }];
+}
+
+@end
