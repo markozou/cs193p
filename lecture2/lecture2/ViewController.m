559bb5201f935782d27e026273a09399de7e34db
markozou <markozou@outlook.com>
2014-11-03 21:55:13 +0800

    lecture2

--- lecture2/lecture2/ViewController.m
+++ lecture2/lecture2/ViewController.m
@@ -2,9 +2,17 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
 @end
 
 @implementation ViewController
+
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -12,6 +20,17 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+        [sender setTitle:@"A♠" forState:UIControlStateNormal];
+    }
+    self.flipCount++;
 }
 
 @end
