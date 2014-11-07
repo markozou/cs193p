69d558ed87a029d528efeb4b9954f5ef25bcfc8d
markozou <markozou@outlook.com>
2014-11-07 22:10:09 +0800

    lecture5

---
+++ lecture5/lecture5Tests/lecture5Tests.m
@@ -0,0 +1,28 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface lecture5Tests : XCTestCase
+
+@end
+
+@implementation lecture5Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+
+    }];
+}
+
+@end
